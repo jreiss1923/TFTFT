3e04cd57919640a4ccd97d4e2cfaf92d2d7cc391
jreiss1923 <joshua.reiss2@gmail.com>
2022-01-02 17:15:11 -0500

    added new flame, fixed refresh out

--- friend_tracker.py
+++ friend_tracker.py
@@ -21,8 +21,9 @@ FLAME_MESSAGE_6 = "sine waving i see you"
 FLAME_MESSAGE_7 = "heyyy hani are you lowering your mmr for easier lobbies?"
 FLAME_MESSAGE_8 = "imagine being lapped by hani"
 FLAME_MESSAGE_9 = "dude clean your room instead of playing"
+FLAME_MESSAGE_10 = "spoken like a true goldie"
 
-FLAME_MESSAGE_LIST_HANI = [FLAME_MESSAGE_1, FLAME_MESSAGE_2, FLAME_MESSAGE_3, FLAME_MESSAGE_4, FLAME_MESSAGE_5, FLAME_MESSAGE_6, FLAME_MESSAGE_7]
+FLAME_MESSAGE_LIST_HANI = [FLAME_MESSAGE_1, FLAME_MESSAGE_2, FLAME_MESSAGE_3, FLAME_MESSAGE_4, FLAME_MESSAGE_5, FLAME_MESSAGE_6, FLAME_MESSAGE_7, FLAME_MESSAGE_10]
 FLAME_MESSAGE_LIST_SANDY = [FLAME_MESSAGE_1, FLAME_MESSAGE_2, FLAME_MESSAGE_3, FLAME_MESSAGE_4, FLAME_MESSAGE_5, FLAME_MESSAGE_8, FLAME_MESSAGE_9]
 
 headers = {"X-Riot-Token": os.environ.get("RIOT_API_TOKEN")}
@@ -180,7 +181,7 @@ async def game_played_tracker():
                 ranking_str = "bot 4"
             else:
                 ranking_str = "top 4"
-            embed = discord.Embed(title=friend + " went " + ranking_str, description=strings[0] + "\n" + strings[3], color=discord.Colour.teal())
+            embed = discord.Embed(title=friend + " went " + ranking_str, description=strings[0] + "\n" + strings[4], color=discord.Colour.teal())
             # displays last comp played
             embed.add_field(name="Last Comp:", value=strings[1] + "\n\n" + strings[2], inline=False)
             await channel_test.send(embed=embed)
